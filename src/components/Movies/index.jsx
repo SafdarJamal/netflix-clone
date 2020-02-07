7667aecbeda670a9899f8cfdfa026bbdf10ec472
Safdar Jamal <safdar.dev@gmail.com>
2020-02-07 22:48:46 +0500

    Connect Movies component to redux store

--- src/components/Movies/index.jsx
+++ src/components/Movies/index.jsx
@@ -1,12 +1,34 @@
-import React from 'react';
-import './style.css';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchMovies } from '../../actions';
+import { Link } from 'react-router-dom';
+import styles from './style.css';
 
-const Movies = () => {
-  return (
-    <div>
-      <h1>Movies</h1>
-    </div>
-  );
+class Movies extends Component {
+  render() {
+    const { children, movies = [], params = {} } = this.props;
+    console.log(this.props);
+
+    return (
+      <div className={styles.movies}>
+        <div className={params.id ? styles.listHidden : styles.list}>
+          {movies.map((movie, index) => (
+            <Link key={index} to={`/movies/${index + 1}`}>
+              <div
+                className={styles.movie}
+                style={{ backgroundImage: `url(${movie.cover})` }}
+              />
+            </Link>
+          ))}
+        </div>
+        {children}
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return { movies: state.movies.all };
 };
 
-export default Movies;
+export default connect(mapStateToProps, { fetchMovies })(Movies);
