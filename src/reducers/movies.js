962f63ab2e9396aae00ff74b1a61de7802852b30
Safdar Jamal <safdar.dev@gmail.com>
2020-02-07 21:35:44 +0500

    Create movies reducer

---
+++ src/reducers/movies.js
@@ -0,0 +1,17 @@
+const initialState = {
+  movies: [],
+  movie: {}
+};
+
+const movies = (state = initialState, action) => {
+  switch (action.key) {
+    case 'FETCH_MOVIES':
+      return { ...state, all: action.movies };
+    case 'FETCH_MOVIE':
+      return { ...state, current: action.movie };
+    default:
+      return state;
+  }
+};
+
+export default movies;
