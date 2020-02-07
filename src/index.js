e2b438feeeef21e83b2379214b88186ffe758692
Safdar Jamal <safdar.dev@gmail.com>
2020-02-07 21:19:14 +0500

    Integrate router

--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './store';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Routes from './routes';
 import './index.css';
-import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router>
+      <Routes />
+    </Router>
   </Provider>,
   document.getElementById('root')
 );
