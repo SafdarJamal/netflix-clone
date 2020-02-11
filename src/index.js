403e94e2cdd21d3bc4dc3ef4a01fe1d592dcc0d3
Safdar Jamal <safdar.dev@gmail.com>
2020-02-11 23:59:31 +0500

    Modify App component

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './store';
 import { BrowserRouter as Router } from 'react-router-dom';
-import Routes from './routes';
+import App from './components/App';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <Provider store={store}>
     <Router>
-      <Routes />
+      <App />
     </Router>
   </Provider>,
   document.getElementById('root')
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 
-import App from '../components/App';
 import Movies from '../components/Movies';
 import Movie from '../components/Movie';
 
 const Routes = () => {
   return (
     <>
-      <Route path="/" component={App} exact />
-      <Route path="/movies/:id" component={Movies} />
+      <Route path="/" component={Movies} exact />
+      <Route path="/movies" component={Movies} />
       <Route path="/movies/:id" component={Movie} />
     </>
   );
